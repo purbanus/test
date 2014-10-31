74a59f89b95aa2dd26db523d7ef3544900ee79c7
Peter Urbanus <purbanus@gmail.com>
2014-10-31 15:12:24 +0100

    HalloWereld toegevoegd

---
+++ Hallootjes 2/src/pu/HalloWereld.java
@@ -0,0 +1,15 @@
+package pu;
+
+public class HalloWereld
+{
+
+	public HalloWereld()
+	{
+	}
+
+	public static void main( String [] args )
+	{
+		System.out.println( "Hallo, wereld, zucht" );
+	}
+
+}
